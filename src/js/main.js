e1c51a925ddb438c69f4c170188c4507c77caf0f
Bruno Baroni <brunobaronims@gmail.com>
2022-08-31 21:44:42 -0300

    Implement quiz fetching

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
+async function getQuizzes() {
+  try {
+    const response = await axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
+    console.log(response.data);
+    return response.data;
+  } catch(error) {
+    console.error('Não foi possível resgatar os quizzes: ', error);
+    return;
+  }
+}
 
+async function renderQuiz(data) {
+  const list = document.querySelector('.Quiz-container');
+  const quiz = document.createElement('div');
+  data.forEach(quiz => {
+
+  });
+}
+
+getQuizzes();
